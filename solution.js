fc1b23e3140a5717db7252bca8494491868304fd
Neil Supillo <neilsupillo@gmail.com>
2024-01-25 18:11:27 +0800

    changes

--- solution.js
+++ solution.js
@@ -34,6 +34,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("./public"));
 
 let currentUserId = 1;
+let message = "";
 
 let users;
 
@@ -47,6 +48,7 @@ async function checkVisisted() {
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
+  console.log(countries);
   return countries;
 }
 
@@ -74,6 +76,7 @@ app.get("/", async (req, res) => {
       total: countries.length,
       users: allUsers,
       color: "",
+      message: message,
     });
   } else {
     res.render("index.ejs", {
@@ -81,8 +84,10 @@ app.get("/", async (req, res) => {
       total: countries.length,
       users: allUsers,
       color: currentUser.color ? currentUser.color : "grey",
+      message: message,
     });
   }
+  message = "";
 });
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
@@ -92,20 +97,31 @@ app.post("/add", async (req, res) => {
     const countryCode = countryNames[input.toLowerCase()];
     console.log(countryCode);
     if (countryCode === undefined) {
+      message = "Not a country";
       res.redirect("/");
       throw new Error("wrong spelling");
     }
     try {
+      const codes = await checkVisisted();
+      codes.forEach((cd) => {
+        if (countryCode === cd) {
+          throw new Error("Country already added");
+        }
+      });
       await db.query(
         "INSERT INTO visited_countries (country_code, user_id) VALUES ($1, $2)",
         [countryCode, currentUserId]
       );
+      message = "Country added";
       res.redirect("/");
     } catch (err) {
+      message = "Country already added";
       console.log("country already added" + err);
+      res.redirect("/");
     }
   } catch (err) {
-    console.log("country name not found or " + err);
+    console.log(err);
+    res.redirect("/");
   }
 });
 
@@ -113,49 +129,58 @@ app.post("/add", async (req, res) => {
 app.post("/user", async (req, res) => {
   //console.log(req.body.user + req.body.add + req.body.edit);
   //console.log(req.body.edit);
-  if (req.body.add === "add") {
-    res.render("new.ejs");
-  } else if (req.body.user) {
+  if (req.body.user) {
     console.log(req.body.user);
     currentUserId = req.body.user;
     res.redirect("/");
-  } else {
-    console.log("he" + req.body.edit);
-    currentUserId = req.body.edit;
-    const currentUser = await getCurrentUser();
-    const countryCode = await checkVisisted();
-    let userCountries = [];
-    countryCode.forEach((code) => {
-      // console.log(countryCodes[code]);
-      // console.log(code);
-      if (countryCodes[code]) {
-        userCountries.push([countryCodes[code], code]);
-      }
-    });
-    //console.log("current");
-    console.log(currentUser);
-    res.render("edit.ejs", {
-      user: currentUser,
-      listTitle: "Country",
-      items: userCountries,
-    });
   }
+  // } else {
+  //   console.log("he" + req.body.edit);
+  //   currentUserId = req.body.edit;
+  //   const currentUser = await getCurrentUser();
+  //   const countryCode = await checkVisisted();
+  //   let userCountries = [];
+  //   countryCode.forEach((code) => {
+  //     // console.log(countryCodes[code]);
+  //     // console.log(code);
+  //     if (countryCodes[code]) {
+  //       userCountries.push([countryCodes[code], code]);
+  //     }
+  //   });
+  //   //console.log("current");
+  //   console.log(currentUser);
+  //   res.render("edit.ejs", {
+  //     user: currentUser,
+  //     listTitle: "Country",
+  //     items: userCountries,
+  //   });
+  // }
 });
 
 //add new user
 app.post("/new", async (req, res) => {
   const name = req.body.name;
   const color = req.body.color;
+  if (!name || !color) {
+    message = "add color";
+    res.redirect("/");
+  }
+  try {
+    const result = await db.query(
+      "INSERT INTO users (name, color) VALUES($1, $2) RETURNING *;",
+      [name, color]
+    );
 
-  const result = await db.query(
-    "INSERT INTO users (name, color) VALUES($1, $2) RETURNING *;",
-    [name, color]
-  );
-
-  const id = result.rows[0].id;
-  currentUserId = id;
-
-  res.redirect("/");
+    const id = result.rows[0].id;
+    currentUserId = id;
+    message = "Added a new user";
+    focus;
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    message = "Name already exist";
+    res.redirect("/");
+  }
 });
 
 // update user account
@@ -166,22 +191,30 @@ app.post("/update", async (req, res) => {
   const ide = req.body.id;
   const color = req.body.color;
   //console.log(name + id + color);
+  if (!name && !color) {
+    message = "add name or add color";
+    res.redirect("/user/account/" + ide);
+  } else {
+    try {
+      if (name) {
+        const results = await db.query(
+          "UPDATE users SET name = ($1) WHERE id = ($2)",
+          [name, ide]
+        );
+      }
+      if (color) {
+        const result = await db.query(
+          "UPDATE users SET color = ($1) WHERE id = ($2)",
+          [color, ide]
+        );
+      }
 
-  try {
-    const results = await db.query(
-      "UPDATE users SET name = ($1) WHERE id = ($2)",
-      [name, ide]
-    );
-    const result = await db.query(
-      "UPDATE users SET color = ($1) WHERE id = ($2)",
-      [color, ide]
-    );
-
-    currentUserId = ide;
-
-    res.redirect("/");
-  } catch (error) {
-    console.log(error);
+      currentUserId = ide;
+      message = "Success";
+      res.redirect("/user/account/" + ide);
+    } catch (error) {
+      console.log(error);
+    }
   }
 });
 
@@ -196,7 +229,7 @@ app.post("/delete", async (req, res) => {
     );
     const results = await db.query("DELETE FROM users WHERE ID = ($1)", [id]);
     currentUserId = 1;
-
+    message = "Account deleted";
     res.redirect("/");
   } catch (error) {
     console.log(error);
@@ -235,12 +268,11 @@ app.post("/edit/country", async (req, res) => {
     });
     console.log("current");
     console.log(currentUser);
-    res.render("edit.ejs", {
-      user: currentUser,
-      listTitle: "Country",
-      items: userCountries,
-    });
+    message = "Country updated";
+    res.redirect("/user/account/" + currentUserId);
   } else {
+    message = "wrong spelling or already exist";
+    res.redirect("/user/account/" + currentUserId);
     console.log("wrong spelling or already exist");
   }
 });
@@ -252,7 +284,7 @@ app.post("/delete/country", async (req, res) => {
       "DELETE FROM visited_countries WHERE user_id = ($1) AND country_code = ($2)",
       [currentUserId, code]
     );
-    const currentUser = users.find((user) => user.id == currentUserId);
+    //const currentUser = users.find((user) => user.id == currentUserId);
     const countryCode = await checkVisisted();
     let userCountries = [];
     countryCode.forEach((code) => {
@@ -264,17 +296,14 @@ app.post("/delete/country", async (req, res) => {
     });
     // console.log("current");
     // console.log(currentUser);
-    res.render("edit.ejs", {
-      user: currentUser,
-      listTitle: "Country",
-      items: userCountries,
-    });
+    message = "Country deleted";
+    res.redirect("/user/account/" + currentUserId);
   } catch (error) {
     console.log(error);
   }
 });
 
-app.get("/user/:id", async (req, res) => {
+app.get("/user/account/:id", async (req, res) => {
   console.log(req.params.id);
   currentUserId = req.params.id;
   const currentUser = await getCurrentUser();
@@ -293,7 +322,9 @@ app.get("/user/:id", async (req, res) => {
     user: currentUser,
     listTitle: "Country",
     items: userCountries,
+    message: message,
   });
+  message = "";
 });
 
 app.get("/fullview/:id", async (req, res) => {
@@ -317,6 +348,10 @@ app.get("/fullview/:id", async (req, res) => {
     total: userCountries.length,
     color: currentUser.color ? currentUser.color : "grey",
   });
+});
+
+app.get("/add/newuser", async (req, res) => {
+  res.render("new.ejs");
 });
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
