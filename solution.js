635a3f7253189ab1658c387a6afde6864234ec92
Neil Supillo <neilsupillo@gmail.com>
2024-02-05 15:39:34 +0800

    dirname

--- solution.js
+++ solution.js
@@ -8,8 +8,8 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("/public"));
-app.set("views", "/views");
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 const POSTGRES_URL =
